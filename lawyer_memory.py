7d813679f8c626fc5c3fa7fd56014ab41d28d785
Duoyun <18029485526@163.com>
2026-08-03 23:18:29 +0800

    add lawyer memory compatibility module

---
+++ lawyer_memory.py
@@ -0,0 +1,12 @@
+class LawyerMemory:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def add(self, *args, **kwargs):
+        pass
+
+    def search(self, *args, **kwargs):
+        return []
+
+    def get(self, *args, **kwargs):
+        return None
